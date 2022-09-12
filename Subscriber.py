b79ae8ebe43b6c4f5d4b909467aa0fa80f4ece97
madhavm0706 <madhavm0706@gmail.com>
2022-09-12 18:33:33 +0530

    modify Publisher and Subscriber.py

--- Subscriber.py
+++ Subscriber.py
@@ -5,6 +5,8 @@ import joblib
 import time
 module  = joblib.load("mlmodule.pk1")
 
+i=1
+
 def on_connect(client,userdata,flags,rc):
     print(f"connected with status code {rc}")
     client.subscribe("raspberry/value")
@@ -12,12 +14,14 @@ def on_connect(client,userdata,flags,rc):
 
 
 def on_message(client,userdata, msg):
-    print(f"{msg.topic} {len(msg.payload)}")
-    time.sleep(10)
-    if(len(msg.payload)):
-        print("hello")
-        client.publish('raspberry/value1', payload="data from subscriber", qos=0, retain=False)
-
+    global i
+    print(f" received data {msg.payload} from publisher via topic {msg.topic} ")
+    
+    
+    print(f"sending  data {i} to publisher via topic raspberry/value1")
+    client.publish('raspberry/value1', payload=i, qos=0, retain=False)
+    i = i+1
+    time.sleep(30)
 
    
 
--- publisher.py
+++ publisher.py
@@ -1,6 +1,13 @@
 import paho.mqtt.client as mqtt
 import time
 
+i=1
+def senddata(a):
+    global i
+    client.publish('raspberry/value', payload=i, qos=0, retain=False)
+    print(f"{a}send data to Subscriber via topic raspberry/value")
+    i =i+1
+
 
 
 
@@ -10,8 +17,10 @@ def on_connect(client,userdata,flags,rc):
     client.subscribe("raspberry/value1")
 
 def on_message(client,userdata,msg):
-    print(f"{msg.topic}{msg.payload}")
-    time.sleep(10)
+    global i
+    print(f"{msg.payload} received data from Subscriber via topic {msg.topic}")
+    senddata(i)
+   
 
 
 
@@ -22,10 +31,11 @@ client.on_message = on_message
 client.connect("localhost",1883,60)
 
 
-a= ["50.2 88.4","51.2 83.4","60.2 78.4","55.2 68.4","10.2 18.4"]
-client.publish('raspberry/value', payload=a[0], qos=0, retain=False)
-print(f"send 1 to raspberry/value")
+# a= ["50.2 88.4","51.2 83.4","60.2 78.4","55.2 68.4","10.2 18.4"]
 
+client.publish('raspberry/value', payload=i, qos=0, retain=False)
+print(f"send data {i} to Subscriber via topic raspberry/value")
+i = i+1
 
 # for i in range(5):
 #     # the four parameters are topic, sending content, QoS and whether retaining the message respectively
